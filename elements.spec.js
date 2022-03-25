e5debe67e328e154ea13f4a9183288f7e9253cfd
ikaeloah <101226956+ikaeloah@users.noreply.github.com>
2022-03-25 12:10:00 -0300

    Update elements.spec.js

--- elements.spec.js
+++ elements.spec.js
@@ -1,8 +1,15 @@
 /// <reference types ="cypress" />
 
 describe('Work with basic elements', () => {
-    it('Text ', () => {
+    before(() => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Text ', () => {
         cy.get('body').should('contain', 'Cuidado')
         // cy.get('body').should('have.text', 'Cuidado')
         cy.get('span').should('contain', 'Cuidado')
@@ -11,8 +18,7 @@ describe('Work with basic elements', () => {
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     })
 
-    it.only('Links', () => {
-        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    it('Links', () => {
         cy.get('[href="#"]').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
 
@@ -20,6 +26,29 @@ describe('Work with basic elements', () => {
         cy.get('#resultado').should('have.not.text', 'Voltou!')
         cy.contains('Voltar').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
+    })
+
+    it.only('TextFields', () => {
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should('have.value', 'Cypress Test')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .type('textarea')
+            .should('have.value', 'textarea')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+        .type('???')
+
+        cy.get('[data-cy="dataSobrenome"]')
+            .type('Teste12345{backspace}{backspace}')
+            .should('have.value', 'Teste123')
+
+            cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', {delay:100})
+            .should('have.value', 'acerto')
+
+
     })
 })
 
