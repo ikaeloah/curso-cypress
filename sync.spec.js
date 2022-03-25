13acfdca7d4720911ba68faedcf1eabad987c214
ikaeloah <101226956+ikaeloah@users.noreply.github.com>
2022-03-25 18:01:40 -0300

    Update sync.spec.js

--- sync.spec.js
+++ sync.spec.js
@@ -18,4 +18,28 @@ describe('Esperas...', () => {
         cy.get('#novoCampo').type('funciona')
     })
 
+    it('Deve fazer retrys', () => {
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#novoCampo')
+            // .should('not.exist')
+            .should('exist')
+            .type('funciona')
+
+    })
+
+    it.only('Uso do find', () => {
+        cy.get('#buttonList').click()
+        cy.get('#Lista li')
+            .find('span')
+            .should('contain', 'Item 1')
+        // cy.get('#Lista li')
+        //     .find('span')
+        //     .should('contain', 'Item 2')
+        cy.get('#lista li span')
+            .should('contain', 'Item 2')
+
+    })
+
 })
