b385c493ce87f13abfa49a5d494bf1464fd5555b
ikaeloah <101226956+ikaeloah@users.noreply.github.com>
2022-03-25 10:26:45 -0300

    Add files via upload

---
+++ describe.spec.js
@@ -0,0 +1,25 @@
+/// <reference types ="cypress" />
+
+it('A external test...', () => {
+
+})
+
+describe('Shold group tests...', () => {
+    describe('Should group more specific tests...', () => {
+        it('A specific test...', () => {
+
+        })
+    })
+
+    describe('Should group more specific tests 2...', () => {
+            it('A specific test 2...', () => {
+                
+            })
+        })
+
+
+
+    it('A internal test...', () => {
+
+    })
+})
