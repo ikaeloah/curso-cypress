ffaf88ec1ac942f749d78b2730027ba1a538058f
ikaeloah <101226956+ikaeloah@users.noreply.github.com>
2022-03-30 15:06:01 -0300

    Add files via upload

---
+++ alert.spec.js
@@ -0,0 +1,47 @@
+/// <reference types ="cypress" />
+
+describe('Work with alerts', () => {
+    before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Alert', () => {
+        cy.get('#alert').click()
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Alert Simples')
+        })
+    })
+
+    it.only('Alert com mock', () => {
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert', stub)
+        cy.get('#alert').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
+        })
+    })
+
+    it.only('Confirm', () => {
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Confirmado')
+        })
+        cy.get('#confirm').click()
+    })
+
+    it.only('Deny', () => {
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+            return false
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Negado')
+        })
+        cy.get('#confirm').click()
+    })
+})
